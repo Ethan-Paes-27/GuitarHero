b1fbf4b2f6858dd9d56e0f8f873f6ca6297d57ee
Ethan-Paes-27 <epaes27@jesuitmail.org>
2025-05-09 15:33:47 -0700

    Update RingBuffer.java
    it works

--- RingBuffer.java
+++ RingBuffer.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  *  methods you need, along with descriptions of what they're supposed to do.
  *
  *  Note: it won't compile until you fill in the constructors and methods
- *        (or at least commment out the ones whose return type is non-void).
+ *        (or at least comment out the ones whose return type is non-void).
  *
  *****************************************************************************/
 
@@ -15,12 +15,12 @@ public class RingBuffer {
     private ArrayList<Double> rb;          // items in the buffer
     private int first;            // index for the next dequeue or peek
     private int last;             // index for the next enqueue
-    private int capacity;             // number of items in the buffer
+    private int maxCap;             // number of items in the buffer
 
     // create an empty buffer, with given max capacity
     public RingBuffer(int capacity) {
         rb = new ArrayList<>(capacity);
-        this.capacity = capacity;
+        maxCap = capacity;
     }
 
     // return number of items currently in the buffer
@@ -35,7 +35,7 @@ public class RingBuffer {
 
     // is the buffer full (size equals array capacity)?
     public boolean isFull() {
-        return size() == capacity;
+        return size() == maxCap;
     }
 
     // add item x to the end
@@ -43,7 +43,7 @@ public class RingBuffer {
         if (isFull()) {
             throw new RuntimeException("Ring buffer overflow");
         }
-        
+
         rb.add(x);
     }
 
@@ -52,7 +52,7 @@ public class RingBuffer {
         if (isEmpty()) {
             throw new RuntimeException("Ring buffer underflow");
         }
-        
+
         return rb.remove(0);
     }
 
@@ -61,13 +61,13 @@ public class RingBuffer {
         if (isEmpty()) {
             throw new RuntimeException("Ring buffer underflow");
         }
-        
+
         return rb.get(0);
     }
 
     // a simple test of the constructor and methods in RingBuffer
     public static void main(String[] args) {
-        int N = 6;
+        int N = 100;
         RingBuffer buffer = new RingBuffer(N);
         for (int i = 1; i <= N; i++) {
             buffer.enqueue(i);
